fc1774746491d5303260ccf1062150b69c3d83cc
cheng-shuai <cheng-shuai@hotmail.com>
2021-03-30 14:41:10 +0800

    完成顶部布局

--- src/views/shop/components/Cart.vue
+++ src/views/shop/components/Cart.vue
@@ -6,7 +6,7 @@
         <div class="check__icon__tag">1</div>
       </div>
       <div class="check__info">
-        总计：<span class="ckeck__info__price">&yen; 128</span>
+        总计：<span class="check__info__price">&yen; 128</span>
       </div>
       <div class="check__btn">
         去结算
@@ -27,24 +27,25 @@ export default {
   left: 0;
   right: 0;
   bottom: 0;
-  height: 49px;
-  background-color: #666;
+  padding: 0;
+  margin: 0;
 }
 .check {
   display: flex;
   &__icon {
     position: relative;
-    text-align: center;
-    line-height: 49px;
     width: 84px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     img {
       width: 28px;
       height: 26px;
     }
     &__tag {
       position: absolute;
-      right: 20px;
-      top: 3px;
+      top: 7px;
+      right: 19px;
       width: 15px;
       height: 15px;
       border-radius: 50%;
@@ -54,6 +55,25 @@ export default {
       line-height: 15px;
       color: #fff;
     }
+  }
+  &__info {
+    flex: 1;
+    font-size: 12px;
+    color: #333;
+    line-height: 49px;
+    &__price {
+      font-size: 18px;
+      line-height: 49px;
+      color: #E93B3B;
+    }
+  }
+  &__btn {
+    width: 98px;
+    font-size: 14px;
+    line-height: 49px;
+    color: #FFFFFF;
+    text-align: center;
+    background-color: #4FB0F9;
   }
 }
 </style>
